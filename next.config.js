3393aad841e45f242befe347662e4b30a71f52d0
web-galtsov <oksana.galtsov@gmail.com>
2022-08-19 14:01:14 +0300

    start project :3000

---
+++ next.config.js
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: [
+      'lh3.googleusercontent.com',
+      'cdn.pixabay.com',
+      'p16-amd-va.tiktokcdn.com',
+      'image.shutterstock.com',
+      'www.yamanashi-kankou.jp'
+    ]
+  }
+};
+
+module.exports = nextConfig;
